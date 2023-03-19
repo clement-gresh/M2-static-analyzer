0aea286d2238be67867ed7dc31bef33bad6c9d51
clement-gresh <clem_paname@hotmail.com>
2023-03-19 12:49:46 +0100

    Added meet, join, min_borne and max_borne methods

--- src/domains/interval_domain.ml
+++ src/domains/interval_domain.ml
@@ -24,12 +24,23 @@ module Intervals = (struct
   (*| NEG_INF, _ | _, NEG_INF -> NEG_INF*)
   | _, _ -> NEG_INF
 
-  
   let sub_borne a b = match a, b with
   | Cst x, Cst y -> Cst (Z.sub x y)
   | NEG_INF, _ | _, POS_INF -> NEG_INF
   (*| _, POS_INF | NEG_INF, _ -> POS_INF*)
   | _, _ -> POS_INF
+
+  let min_borne a b = match a, b with
+  | NEG_INF, _ | _, NEG_INF -> NEG_INF
+  | POS_INF, x | x, POS_INF -> x
+  | x, y when x > y -> y
+  | x, _ -> x
+  
+  let max_borne a b = match a, b with
+  | POS_INF, _ | _, POS_INF -> POS_INF
+  | NEG_INF, x | x, NEG_INF -> x
+  | x, y when x > y -> x
+  | _, x -> x
 
   let add a b = match a, b with
   | BOT, _ | _, BOT -> BOT
@@ -38,6 +49,16 @@ module Intervals = (struct
   let sub a b = match a, b with
   | BOT, _ | _, BOT -> BOT
   | INTERVAL(x1, x2), INTERVAL(y1, y2) -> INTERVAL(sub_borne x1 y2, sub_borne x2 y1)
+  
+  let join a b = match a, b with
+  | BOT, x | x, BOT -> x
+  | INTERVAL(x1, x2), INTERVAL(y1, y2) -> INTERVAL(min_borne x1 y1, max_borne x2 y2)
+
+  let meet a b = match a, b with
+  | INTERVAL(x1, x2), INTERVAL(y1, y2) when x2 >= y1 || x1 <= y2 -> INTERVAL(max_borne x1 y1, min_borne x2 y2)
+  (*| BOT, _ | _, BOT -> BOT
+  | INTERVAL(x1, x2), INTERVAL(y1, y2) when x2 < y1 || x1 > y2 -> BOT*)
+  | _, _ -> BOT
 
   (*debug start*)
   let bwd_binary = assert false
@@ -49,8 +70,6 @@ module Intervals = (struct
   let print = assert false
   let is_bottom = assert false
   let subset = assert false
-  let meet = assert false
-  let join = assert false
   let rand = assert false
   (*debug end*)
 
