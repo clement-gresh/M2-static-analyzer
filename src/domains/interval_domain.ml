c1226368e525302c025d3801205d5b61a3a7591a
clement-gresh <clem_paname@hotmail.com>
2023-03-21 11:46:47 +0100

    added neg for interval domain + copy/paste most of the code from constant domain

--- src/domains/interval_domain.ml
+++ src/domains/interval_domain.ml
@@ -1,4 +1,4 @@
-(*open Abstract_syntax_tree*)
+open Abstract_syntax_tree
 open Value_domain
 
 module Intervals = (struct
@@ -17,6 +17,11 @@ module Intervals = (struct
   let top = INTERVAL(NEG_INF, POS_INF)
   
   let const c = INTERVAL(Cst c, Cst c)
+
+  let neg_borne a = match a with
+  | POS_INF -> NEG_INF
+  | NEG_INF -> POS_INF
+  | Cst x -> Cst (Z.neg x)
 
   let add_borne a b = match a, b with
   | Cst x, Cst y -> Cst (Z.add x y)
@@ -70,6 +75,10 @@ module Intervals = (struct
   | INTERVAL(x1, x2), INTERVAL(y1, y2) when geq_borne x1 y1 && geq_borne y2 x2 -> true
   | _, _ -> false
 
+  let neg a = match a with
+  | INTERVAL(x, y) -> INTERVAL(neg_borne y, neg_borne x)
+  | _ -> BOT
+
   let add a b = match a, b with
   | BOT, _ | _, BOT -> BOT
   | INTERVAL(x1, x2), INTERVAL(y1, y2) -> INTERVAL(add_borne x1 y1, add_borne x2 y2)
@@ -112,6 +121,8 @@ module Intervals = (struct
   | _, _ -> BOT, BOT
 
   (*debug start*)
+  let lift1 = assert false
+  let lift2 = assert false
   let bwd_binary = assert false
   let bwd_unary = assert false
   let compare = assert false
@@ -119,9 +130,54 @@ module Intervals = (struct
   let binary = assert false
   let unary = assert false
   let is_bottom = assert false (*ok*)
-  let subset = assert false (*subset a b checks if a is included in b*)
   let rand = assert false
   (*debug end*)
 
+  
+  (* operator dispatch *)
+
+  let unary x op = match op with
+  | AST_UNARY_PLUS  -> x
+  | AST_UNARY_MINUS -> neg x
+
+  let binary x y op = match op with
+  | AST_PLUS     -> add x y
+  | AST_MINUS    -> sub x y
+  | AST_MULTIPLY -> mul x y
+  | AST_DIVIDE   -> div x y
+
+  let compare x y op = match op with
+  | AST_EQUAL         -> eq x y
+  | AST_NOT_EQUAL     -> neq x y
+  | AST_GREATER_EQUAL -> geq x y
+  | AST_GREATER       -> gt x y
+  | AST_LESS_EQUAL    -> let y',x' = geq y x in x',y'
+  | AST_LESS          -> let y',x' = gt y x in x',y'
+
+
+  let bwd_unary x op r = match op with
+  | AST_UNARY_PLUS  -> meet x r
+  | AST_UNARY_MINUS -> meet x (neg r)
+
+
+  let bwd_binary x y op r = match op with
+
+  | AST_PLUS ->
+      (* r=x+y => x=r-y and y=r-x *)
+      meet x (sub r y), meet y (sub r x)
+
+  | AST_MINUS ->
+      (* r=x-y => x=y+r and y=x-r *)
+      meet x (add y r), meet y (sub x r)
+
+  | AST_MULTIPLY ->
+      (* r=x*y => (x=r/y or y=r=0) and (y=r/x or x=r=0)  *)
+      let contains_zero o = subset (const Z.zero) o in
+      (if contains_zero y && contains_zero r then x else meet x (div r y)),
+      (if contains_zero x && contains_zero r then y else meet y (div r x))
+
+  | AST_DIVIDE ->
+      (* this is sound, but not precise *)
+      x, y
   
 end : VALUE_DOMAIN)
