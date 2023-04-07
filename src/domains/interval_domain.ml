d199b54425bd018bb2174f66e30565cd2f70af9f
clement-gresh <clem_paname@hotmail.com>
2023-04-07 11:35:06 +0200

    Layout improvments

--- src/domains/interval_domain.ml
+++ src/domains/interval_domain.ml
@@ -18,6 +18,14 @@ module Intervals = (struct
   
   let const c = INTERVAL(Cst c, Cst c) (* debug : a verifier *)
 
+  let rand x y =
+        if x=y then const x (* [x,x] *)
+        else if x<y then INTERVAL(Cst x, Cst y) (* [x ,y]*)
+        else BOT
+
+
+  (* operations on bounds *)
+
   let print_borne a = match a with
   | NEG_INF -> "-∞"
   | POS_INF -> "+∞"
@@ -58,12 +66,6 @@ module Intervals = (struct
   | Cst x, Cst y when x >= y -> true
   | _, _ -> false
 
-  let meet a b = match a, b with
-  | INTERVAL(x1, x2), INTERVAL(y1, y2) when geq_borne x2 y1 || geq_borne y2 x1 -> INTERVAL(max_borne x1 y1, min_borne x2 y2)
-  (*| BOT, _ | _, BOT -> BOT
-  | INTERVAL(x1, x2), INTERVAL(y1, y2) when x2 < y1 || x1 > y2 -> BOT*)
-  | _, _ -> BOT
-
   let gt_borne a b = match a, b with
   | x, Cst y -> geq_borne x (add_borne (Cst y) (Cst Z.one))
   | x, y -> geq_borne x y   (*debug : is POS_INF strictly greater than POS_INF ?*)
@@ -71,114 +73,44 @@ module Intervals = (struct
   | POS_INF, _ | _, NEG_INF -> false
   | Cst x, Cst y when x < y -> true
   | _, _ -> false *)
-    
-let eq z u =
-match z, u with
-| INTERVAL(NEG_INF, POS_INF), x | x, INTERVAL(NEG_INF, POS_INF) -> x, x
-| BOT, _ | _, BOT -> BOT, BOT
-| _ -> let intersection = meet z u in
-        intersection, intersection
-
-let sub_one borne =
-match borne with
-| Cst x -> Cst (Z.sub x Z.one)
-| _ -> borne
-
-let add_one borne =
-match borne with
-| Cst x -> Cst (Z.add x Z.one)
-| _ -> borne
-
-let neq z u =
-match z, u with
-| INTERVAL(NEG_INF, POS_INF), _ | _, INTERVAL(NEG_INF, POS_INF) | BOT, _ | _, BOT -> z, u
-| INTERVAL(a, b), INTERVAL(c, d) ->
-        if a = b then
-                if c = d then
-                        if a = c then
-                                BOT, BOT
-                        else
-                                z, u
-                else if a = c then
-                        z, INTERVAL(add_one c, d)
-                else if a = d then
-                        z, INTERVAL(c, sub_one d)
-                else
-                        z, u
-        else if c = d then
-                if c = a then
-                        INTERVAL(add_one a, b), u
-                else if c = b then
-                        INTERVAL(a, sub_one b), u
-                else
-                        z, u
-        else
-                z, u
+  
+        let sub_one borne =
+        match borne with
+        | Cst x -> Cst (Z.sub x Z.one)
+        | _ -> borne
         
-  let gt_zero a = match a with
-	| Cst c -> c > Z.zero
-	| _ -> false
-
-  let mul_bornes borneA borneB =
-    match borneA, borneB with
-    | Cst x, Cst y -> Cst (Z.mul x y)
-    | NEG_INF, POS_INF | POS_INF, NEG_INF -> NEG_INF
-    | NEG_INF, NEG_INF -> POS_INF
-    | POS_INF, Cst x | Cst x, POS_INF ->
+        let add_one borne =
+        match borne with
+        | Cst x -> Cst (Z.add x Z.one)
+        | _ -> borne
+                
+        let gt_zero a = match a with
+        | Cst c -> c > Z.zero
+        | _ -> false
+        
+        let mul_bornes borneA borneB =
+        match borneA, borneB with
+        | Cst x, Cst y -> Cst (Z.mul x y)
+        | NEG_INF, POS_INF | POS_INF, NEG_INF -> NEG_INF
+        | NEG_INF, NEG_INF -> POS_INF
+        | POS_INF, Cst x | Cst x, POS_INF ->
             if x = Z.zero then Cst Z.zero
             else if x < Z.zero then NEG_INF else POS_INF
-    | NEG_INF, Cst x | Cst x, NEG_INF ->
+        | NEG_INF, Cst x | Cst x, NEG_INF ->
             if x = Z.zero then Cst Z.zero
             else if x > Z.zero then NEG_INF else POS_INF
-    | POS_INF, POS_INF -> POS_INF
+        | POS_INF, POS_INF -> POS_INF
+        
+        (* Le cas ou l'une des bornes est nulle est déjà traitée dans apply_diff_zero *)
+        let div_bornes borneA borneB =
+        match borneA, borneB with
+        | Cst x, Cst y -> Cst (Z.div x y)
+        | _, POS_INF | _, NEG_INF -> Cst(Z.zero)
+        | POS_INF, Cst x -> if x < Z.zero then NEG_INF else POS_INF
+        | NEG_INF, Cst x -> if x > Z.zero then NEG_INF else POS_INF
 
-(* Le cas ou l'une des bornes est nulle est déjà traitée dans apply_diff_zero *)
-let div_bornes borneA borneB =
-    match borneA, borneB with
-    | Cst x, Cst y -> Cst (Z.div x y)
-    | _, POS_INF | _, NEG_INF -> Cst(Z.zero)
-    | POS_INF, Cst x -> if x < Z.zero then NEG_INF else POS_INF
-    | NEG_INF, Cst x -> if x > Z.zero then NEG_INF else POS_INF
-    
-  let mul z u =
-    match z, u with
-    | BOT , _  | _, BOT -> BOT
-    | INTERVAL(NEG_INF, POS_INF)  , _ | _, INTERVAL(NEG_INF, POS_INF)  -> INTERVAL(NEG_INF, POS_INF) 
-    | INTERVAL(a, b), INTERVAL(c, d) ->
-            let ac = mul_bornes a c in
-            let ad = mul_bornes a d in
-            let bc = mul_bornes b c in
-            let bd = mul_bornes b d in
-            let borne_inf = min (min ac ad) (min bc bd) in
-            let borne_max = max (max ac ad) (max bc bd) in
-            INTERVAL(borne_inf, borne_max)
-
-let div z u =
-    match z, u with
-    | BOT , _  | _, BOT -> BOT
-    | INTERVAL(NEG_INF, POS_INF)  , _ | _, INTERVAL(NEG_INF, POS_INF)  -> INTERVAL(NEG_INF, POS_INF) 
-    | INTERVAL (_, _), INTERVAL(Cst x, _) when x = Z.zero -> BOT
-    | INTERVAL (_, _), INTERVAL(_, Cst x) when x = Z.zero -> BOT
-    | INTERVAL(a, b), INTERVAL(c, d) ->
-            let ac = div_bornes a c in
-            let ad = div_bornes a d in
-            let bc = div_bornes b c in
-            let bd = div_bornes b d in
-            let borne_inf = if gt_zero c then
-                    min ac ad else min bc bd in
-            let borne_sup = if gt_zero c then
-                    max bc bd else max ac ad in
-            INTERVAL(borne_inf, borne_sup)
-
-  let print fmt x = match x with
-  | BOT -> Format.fprintf fmt "⊥"
-  | INTERVAL(x, y) -> Format.fprintf fmt "[%s;%s]" (print_borne x) (print_borne y)
-
-  let subset a b = match a,b with
-  | BOT, _ | _, INTERVAL(NEG_INF, POS_INF) -> true
-  | INTERVAL(x1, x2), INTERVAL(y1, y2) when geq_borne x1 y1 && geq_borne y2 x2 -> true
-  | _, _ -> false
-
+  
+  (* arithmetic operations *)
   let neg a = match a with
   | INTERVAL(x, y) -> INTERVAL(neg_borne y, neg_borne x)
   | _ -> BOT
@@ -191,10 +123,95 @@ let div z u =
   | BOT, _ | _, BOT -> BOT
   | INTERVAL(x1, x2), INTERVAL(y1, y2) -> INTERVAL(sub_borne x1 y2, sub_borne x2 y1)
   
+  let mul z u =
+        match z, u with
+        | BOT , _  | _, BOT -> BOT
+        | INTERVAL(NEG_INF, POS_INF)  , _ | _, INTERVAL(NEG_INF, POS_INF)  -> INTERVAL(NEG_INF, POS_INF) 
+        | INTERVAL(a, b), INTERVAL(c, d) ->
+                let ac = mul_bornes a c in
+                let ad = mul_bornes a d in
+                let bc = mul_bornes b c in
+                let bd = mul_bornes b d in
+                let borne_inf = min (min ac ad) (min bc bd) in
+                let borne_max = max (max ac ad) (max bc bd) in
+                INTERVAL(borne_inf, borne_max)
+    
+  let div z u =
+        match z, u with
+        | BOT , _  | _, BOT -> BOT
+        | INTERVAL(NEG_INF, POS_INF)  , _ | _, INTERVAL(NEG_INF, POS_INF)  -> INTERVAL(NEG_INF, POS_INF) 
+        | INTERVAL (_, _), INTERVAL(Cst x, _) when x = Z.zero -> BOT
+        | INTERVAL (_, _), INTERVAL(_, Cst x) when x = Z.zero -> BOT
+        | INTERVAL(a, b), INTERVAL(c, d) ->
+                let ac = div_bornes a c in
+                let ad = div_bornes a d in
+                let bc = div_bornes b c in
+                let bd = div_bornes b d in
+                let borne_inf = if gt_zero c then
+                        min ac ad else min bc bd in
+                let borne_sup = if gt_zero c then
+                        max bc bd else max ac ad in
+                INTERVAL(borne_inf, borne_sup)
+    
+
+  (* set-theoretic operations *)
+  
+  let meet a b = match a, b with
+  | INTERVAL(x1, x2), INTERVAL(y1, y2) when geq_borne x2 y1 || geq_borne y2 x1 -> INTERVAL(max_borne x1 y1, min_borne x2 y2)
+  (*| BOT, _ | _, BOT -> BOT
+  | INTERVAL(x1, x2), INTERVAL(y1, y2) when x2 < y1 || x1 > y2 -> BOT*)
+  | _, _ -> BOT
+  
   let join a b = match a, b with
   | BOT, x | x, BOT -> x
   | INTERVAL(x1, x2), INTERVAL(y1, y2) -> INTERVAL(min_borne x1 y1, max_borne x2 y2)
 
+  let widen x y = match x, y with 
+  | BOT, a | a, BOT -> a
+  | INTERVAL (a, b), INTERVAL (c, d) -> 
+      if geq_borne c a then 
+        (if geq_borne b d then INTERVAL (a, b)
+        else INTERVAL (a, POS_INF))
+      else 
+        (if geq_borne b d then INTERVAL (NEG_INF, b)
+        else INTERVAL (NEG_INF, POS_INF))
+
+
+  (* comparison operations (filters) *)
+
+        let eq z u =
+        match z, u with
+        | INTERVAL(NEG_INF, POS_INF), x | x, INTERVAL(NEG_INF, POS_INF) -> x, x
+        | BOT, _ | _, BOT -> BOT, BOT
+        | _ -> let intersection = meet z u in
+                intersection, intersection
+
+        let neq z u =
+        match z, u with
+        | INTERVAL(NEG_INF, POS_INF), _ | _, INTERVAL(NEG_INF, POS_INF) | BOT, _ | _, BOT -> z, u
+        | INTERVAL(a, b), INTERVAL(c, d) ->
+                if a = b then
+                        if c = d then
+                                if a = c then
+                                        BOT, BOT
+                                else
+                                        z, u
+                        else if a = c then
+                                z, INTERVAL(add_one c, d)
+                        else if a = d then
+                                z, INTERVAL(c, sub_one d)
+                        else
+                                z, u
+                else if c = d then
+                        if c = a then
+                                INTERVAL(add_one a, b), u
+                        else if c = b then
+                                INTERVAL(a, sub_one b), u
+                        else
+                                z, u
+                else
+                        z, u
+        
   (*let leq a b = match a, b with
   | INTERVAL(x1, x2), INTERVAL(y1, y2) when geq_borne y2 x1 -> INTERVAL(x1, min_borne x2 y2), INTERVAL(max_borne x1 y1, y2)
   | _, _ -> BOT, BOT*)
@@ -211,32 +228,32 @@ let div z u =
   | BOT, _ | _, BOT -> BOT, BOT*)
   | _, _ -> BOT, BOT
 
-  (*debug : à vérifier*)
   let gt (a:t) (b:t) : t*t = match a, b with
   (* | INTERVAL(x1, x2), INTERVAL(y1, y2) -> geq INTERVAL(add_borne x1 (Cst Z.one), add_borne x2 (Cst Z.one)) INTERVAL(y1, y2) *)
   | INTERVAL(x1, x2), INTERVAL(y1, y2) when gt_borne x2 y1 -> INTERVAL(max_borne x1 (add_borne y1 (Cst Z.one)), x2),
                                                         INTERVAL(y1, min_borne (sub_borne x2 (Cst Z.one)) y2)
   | _, _ -> BOT, BOT
   
-  (* operator dispatch *)
-  (* let widen = join *)
-  let widen x y = match x, y with 
-  | BOT, a | a, BOT -> a
-  | INTERVAL (a, b), INTERVAL (c, d) -> 
-      if geq_borne c a then 
-        (if geq_borne b d then INTERVAL (a, b)
-        else INTERVAL (a, POS_INF))
-      else 
-        (if geq_borne b d then INTERVAL (NEG_INF, b)
-        else INTERVAL (NEG_INF, POS_INF))
 
-  let rand x y =
-    if x=y then const x (* [x,x] *)
-    else if x<y then INTERVAL(Cst x, Cst y) (* [x ,y]*)
-    else BOT
+  (* subset inclusion of concretizations *)
+  let subset a b = match a,b with
+  | BOT, _ | _, INTERVAL(NEG_INF, POS_INF) -> true
+  | INTERVAL(x1, x2), INTERVAL(y1, y2) when geq_borne x1 y1 && geq_borne y2 x2 -> true
+  | _, _ -> false
 
+
+  (* print abstract element *)
+  let print fmt x = match x with
+  | BOT -> Format.fprintf fmt "⊥"
+  | INTERVAL(x, y) -> Format.fprintf fmt "[%s;%s]" (print_borne x) (print_borne y)
+
+  
+  (* check the emptiness of the concretization *)
   let is_bottom a =
     a=BOT
+
+
+  (* operator dispatch *)
 
   let unary x op = match op with
   | AST_UNARY_PLUS  -> x
