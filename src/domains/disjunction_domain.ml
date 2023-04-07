95d2a3575e7ef64a3785b0cf633e164c369dd469
clement-gresh <clem_paname@hotmail.com>
2023-04-07 20:02:45 +0200

    implemented meet for disjunctions

--- src/domains/disjunction_domain.ml
+++ src/domains/disjunction_domain.ml
@@ -32,20 +32,23 @@ module Disjunctions(D:DOMAIN) = (struct         (* domain est non-relationnal*)
     let compare = assert false (* rec calls to compare ? Checks what has been done in other domains, like intervals *)
 
     (* abstract join *)
-    let join = assert false (* join arbre1 arbre 2 = DISJ(arbre1, arbre2) *)
+    let join (a:t) (b:t) : t = DISJ(a, b) (* join arbre1 arbre 2 = DISJ(arbre1, arbre2) *)
 
     (* abstract intersection *)
-    let meet = assert false
-    (* on fait le meet de chacun des feuilles de gacuhe avec chacune des feuilles de droite
-       si on obtient bottom, on rejette le résultat
-       sinon on l'ajoute.
+    let rec meet (a:t) (b:t) : t = match a, b with
+      | SIMPLE x, SIMPLE y -> SIMPLE(D.meet x y)  (* debug: si on on a bottom, on rejette le résultat : comment faire ça *)
+      | SIMPLE x, DISJ(y1, y2) -> DISJ(meet (SIMPLE x) y1, meet (SIMPLE x) y2)
+      | DISJ(x1, x2), SIMPLE y -> DISJ(meet x1 (SIMPLE y), meet x2 (SIMPLE y))
+      | DISJ(x1, x2), DISJ(y1, y2) -> DISJ(DISJ(meet x1 y1, meet x1 y2), DISJ(meet x2 y1, meet x2 y2))
+    (* on fait le meet de chacun des feuilles de gacuhe avec chacune des feuilles de droite.
+       Si on obtient bottom, on rejette le résultat. Sinon on l'ajoute.
        On se retrouve dans le pire cas avec un nombre quadratique de feuilles *)
 
     (* widening *)
     let widen = assert false
 
     (* whether an abstract element is included in another one *)
-    let subset = assert false  (* cours 11, slide 25 : comment faire le subset ainsi que ses limites *)
+    let subset = assert false  (* cours 11, slide 25 : comment faire le subset ainsi que la description des limites du subset *)
 
     (* whether the abstract element represents the empty set *)
     let is_bottom = assert false
